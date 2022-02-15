161fff597b208e0971111e8a0da8c5f25919e328
Johan Munetón <97465949+johanmudea@users.noreply.github.com>
2022-02-14 20:18:39 -0500

    Update Practice4.java
    Lee un número por teclado y comprueba que este numero es mayor o igual que cero, si no lo es lo volverá a pedir (do while), después muestra ese número por consola.
    Crea una aplicación por consola que nos pida un día de la semana y que nos diga si es un día laboral o no. Usa un switch para ello.

--- Practice1/src/co/com/jm/Practice4.java
+++ Practice1/src/co/com/jm/Practice4.java
@@ -1,6 +1,10 @@
 package co.com.jm;
 
 import java.util.Scanner;
+/**
+7. Lee un número por teclado y comprueba que este numero es mayor o igual que cero, si no lo es lo volverá a pedir (do while), después muestra ese número por consola.
+8. Crea una aplicación por consola que nos pida un día de la semana y que nos diga si es un día laboral o no. Usa un switch para ello.
+*/
 
 public class Practice4 {
 
